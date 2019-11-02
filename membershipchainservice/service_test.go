cf99f8ef063c338979c9950baedff476d66982dc
Arnaud Pannatier <arn.pannatier@gmail.com>
2019-11-02 13:38:31 +0100

    Rewrite Map using ServerIdentity.

--- membershipchainservice/service_test.go
+++ membershipchainservice/service_test.go
@@ -6,7 +6,8 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"go.dedis.ch/kyber/v3/suites"
-	"go.dedis.ch/onet"
+	"go.dedis.ch/onet/network"
+	"go.dedis.ch/onet/v3"
 	"go.dedis.ch/onet/v3/log"
 )
 
@@ -21,14 +22,13 @@ func TestSetGenesisSigners(t *testing.T) {
 
 	// Generate 10 nodes, the first 2 are the first signers
 	nbrNodes := 10
-	hosts, _, _ := local.GenTree(nbrNodes, true)
+	hosts, roster, _ := local.GenTree(nbrNodes, true)
 	defer local.CloseAll()
-
 	services := local.GetServices(hosts, membershipID)
 
-	signers := map[string]bool{
-		"Test":  true,
-		"Test2": true,
+	signers := map[*network.ServerIdentity]bool{
+		hosts[0].ServerIdentity: true,
+		hosts[1].ServerIdentity: true,
 	}
 
 	for _, s := range services {
@@ -41,6 +41,34 @@ func TestSetGenesisSigners(t *testing.T) {
 
 		reply = service.GetSigners()
 		assert.Equal(t, reply.Set, signers)
+
+	}
+}
+
+func TestRegisterNewSigners(t *testing.T) {
+	local := onet.NewTCPTest(tSuite)
+
+	// Generate 10 nodes, the first 2 are the first signers
+	nbrNodes := 10
+	hosts, _, _ := local.GenTree(nbrNodes, true)
+	defer local.CloseAll()
+	services := local.GetServices(hosts, membershipID)
+
+	signers := map[*onet.Server]bool{
+		hosts[0]: true,
+		hosts[1]: true,
+	}
+
+	for _, s := range services {
+		service := s.(*Service)
+		service.SetGenesisSigners(signers)
+	}
+
+	assert.NoError(t, services[2].(*Service).Registrate())
+	for _, s := range services {
+		service := s.(*Service)
+		reply := service.GetSigners()
+		assert.True(t, reply.Set[hosts[2]])
 
 	}
 
