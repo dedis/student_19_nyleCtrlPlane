734c361c92a5be8d716b4090297994a995efe307
Arnaud Pannatier <arn.pannatier@gmail.com>
2019-12-06 10:44:58 +0100

    Revert "Add ticker to the Cycle class."
    This reverts commit 26a98a6b60096f6b9beec8817654b7d1223fb3a1.

--- membershipchainservice/cycleclock.go
+++ membershipchainservice/cycleclock.go
@@ -1,10 +1,6 @@
 package membershipchainservice
 
-import (
-	"time"
-
-	"go.dedis.ch/onet/v3/log"
-)
+import "time"
 
 type Phase int
 
@@ -18,11 +14,8 @@ const (
 
 // Cycle describes a sequence of repeating phases starting at a given start time
 type Cycle struct {
-	Sequence         []time.Duration
-	StartTime        time.Time
-	RegistrationTick *time.Ticker
-	EpochTick        *time.Ticker
-	Done             chan bool
+	Sequence  []time.Duration
+	StartTime time.Time
 }
 
 // TotalCycleTime return the total time of a Cycle
@@ -61,46 +54,4 @@ func (c Cycle) GetTimeTillNextEpoch() time.Duration {
 // GetEpoch will give the current epoch based on the clock cycle
 func (c Cycle) GetEpoch() Epoch {
 	return Epoch(time.Now().Sub(c.StartTime) / c.TotalCycleTime())
-}
-
-// StartTicking instanciate the tickers
-func (c Cycle) StartTicking(registrationFn, epochFn, callback func()) {
-	c.Done = make(chan bool)
-	log.LLvl1("Set Registration Tick")
-	c.StartTime = time.Now()
-	c.RegistrationTick = time.NewTicker(c.TotalCycleTime())
-	time.Sleep(c.GetTimeTillNextEpoch())
-	log.LLvl1("Set Epoch Tick")
-	c.EpochTick = time.NewTicker(c.TotalCycleTime())
-
-	go func() {
-		for {
-			select {
-			case <-c.RegistrationTick.C:
-				registrationFn()
-			case <-c.EpochTick.C:
-				epochFn()
-			case <-c.Done:
-				log.LLvl1("Ticking done.")
-				return
-			}
-		}
-	}()
-
-	callback()
-}
-
-// StopTicking stops the tickers
-func (c Cycle) StopTicking() {
-	log.LLvl1(c)
-	if c.RegistrationTick != nil {
-		c.RegistrationTick.Stop()
-	}
-	if c.EpochTick != nil {
-		c.EpochTick.Stop()
-	}
-	log.LLvl1("Stop Ticking")
-	c.Done <- true
-	close(c.Done)
-
 }
