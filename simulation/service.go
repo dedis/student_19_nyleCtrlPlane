9884311611b7d5a2cb0ee85cd632630b848ff16e
Arnaud Pannatier <arn.pannatier@gmail.com>
2019-12-09 16:04:23 +0100

    Add a false implementation of global simulation tests.

--- simulation/service.go
+++ simulation/service.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/BurntSushi/toml"
-	nylechain "github.com/dedis/student_19_nyleCtrlPlane"
+	"github.com/dedis/student_19_nyleCtrlPlane/membershipchainservice"
 	"go.dedis.ch/onet/v3"
 	"go.dedis.ch/onet/v3/log"
-	"go.dedis.ch/onet/v3/simul/monitor"
+	"go.dedis.ch/onet/v3/network"
 )
 
 /*
@@ -49,26 +51,59 @@ func (s *SimulationService) Setup(dir string, hosts []string) (
 // SimulationBFTree structure which will load the roster- and the
 // tree-structure to speed up the first round.
 func (s *SimulationService) Node(config *onet.SimulationConfig) error {
-	index, _ := config.Roster.Search(config.Server.ServerIdentity.ID)
-	if index < 0 {
-		log.Fatal("Didn't find this node in roster")
-	}
-	log.Lvl3("Initializing node-index", index)
+	name := GetServerIdentityToName(config.Server.ServerIdentity, config.Roster)
+	log.LLvl3("Initializing node-index", name)
+
+	myservice := config.GetService(membershipchainservice.ServiceName).(*membershipchainservice.Service)
+	myservice.Name = name
+
 	return s.SimulationBFTree.Node(config)
+
 }
 
 // Run is used on the destination machines and runs a number of
 // rounds
 func (s *SimulationService) Run(config *onet.SimulationConfig) error {
 	size := config.Tree.Size()
-	log.Lvl2("Size is:", size, "rounds:", s.Rounds)
-	_ = nylechain.NewClient()
 
-	for round := 0; round < s.Rounds; round++ {
-		log.Lvl1("Starting round", round)
-		round := monitor.NewTimeMeasure("round")
+	nbrFirstSigners := 4
 
-		round.Record()
+	servers := make(map[*network.ServerIdentity]string)
+
+	for i := 0; i < nbrFirstSigners; i++ {
+		si := config.Roster.List[i]
+		servers[si] = GetServerIdentityToName(si, config.Roster)
+		log.LLvl1("Signers 0 : ", si)
 	}
+
+	myservice := config.GetService(membershipchainservice.ServiceName).(*membershipchainservice.Service)
+	log.Lvl2("Size is:", size, "my name", myservice.Name)
+	myservice.SetGenesisSigners(servers)
+
+	err := myservice.CreateProofForEpoch(1)
+	if err != nil {
+		return err
+	}
+
+	if myservice.Name == "node_0" {
+		err = myservice.GetConsencusOnNewSigners()
+		if err != nil {
+			return err
+		}
+	}
+	err = myservice.StartNewEpoch()
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+//GetServerIdentityToName translate a SI from the global roster to its name
+func GetServerIdentityToName(si *network.ServerIdentity, roster *onet.Roster) string {
+	index, _ := roster.Search(si.ID)
+	if index < 0 {
+		log.Fatal("Didn't find this node in roster")
+	}
+	return "node_" + strconv.Itoa(index)
 }
